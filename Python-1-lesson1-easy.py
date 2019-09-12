d978cd328c6bcdbbd7d171c21d4f5cf8f26e93ab
vladburba <vlad.burba@gmail.com>
2019-09-13 00:35:52 +0300

    Сделал 3 задание закончил изи

--- Python-1-lesson1-easy.py
+++ Python-1-lesson1-easy.py
@@ -12,3 +12,10 @@ do = int(input("Введите число: "))
 print('Вы ввели число: ', do, 'прибавим к нему 2')
 do = do + 2
 print('Результат равен: ', do)
+print('Task 2 is finished')
+age = int(input('Введите ваш возраст:'))
+if age >= 18:
+    print("Доступ разрешен")
+else:
+    print("Извините, пользование данным ресурсом только с 18 лет")
+print('Task 3 is finished')

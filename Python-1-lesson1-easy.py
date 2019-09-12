58e87245707a5d86134d162f13a7420f5fae8b25
vladburba <vlad.burba@gmail.com>
2019-09-13 00:16:58 +0300

    Урок первый сделал задача 1 и задача 2

---
+++ Python-1-lesson1-easy.py
@@ -0,0 +1,14 @@
+a = 1
+b = 2
+c = 3
+print('a,b,c =',a,b,c)
+a,b,c = 3,2,1
+print('поменяли значения ',a,b,c)
+print("Привет, программист!")
+name = input("Ваше имя: ")
+print(name, ", добро пожаловать в мир Python!")
+print('Task 1 is finished')
+do = int(input("Введите число: "))
+print('Вы ввели число: ', do, 'прибавим к нему 2')
+do = do + 2
+print('Результат равен: ', do)

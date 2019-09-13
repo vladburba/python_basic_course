25f0e0734f78b1a62215d51aac9836f53c0f927e
vladburba <vlad.burba@gmail.com>
2019-09-13 12:19:31 +0300

    Добработал первое задание

--- Python-1-lesson1-easy.py
+++ Python-1-lesson1-easy.py
@@ -1,12 +1,10 @@
-a = 1
-b = 2
-c = 3
+print("Привет, программист!")
+name = input("Введите ваше имя: ")
+print(name, ", добро пожаловать в мир Python!")
+a,b,c, = 1,2,3
 print('a,b,c =',a,b,c)
 a,b,c = 3,2,1
-print('поменяли значения ',a,b,c)
-print("Привет, программист!")
-name = input("Ваше имя: ")
-print(name, ", добро пожаловать в мир Python!")
+print('a,b,c =',a,b,c)
 print('Task 1 is finished')
 do = int(input("Введите число: "))
 print('Вы ввели число: ', do, 'прибавим к нему 2')

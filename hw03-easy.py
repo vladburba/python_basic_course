ff190ac9a37139e771d823c7fbf217a4157cc7bf
vladburba <vlad.burba@gmail.com>
2019-09-19 20:08:02 +0300

    Сделал изи 2

--- hw03-easy.py
+++ hw03-easy.py
@@ -10,3 +10,20 @@ name = input('Input name: ')
 age = input('Input age: ')
 town = input('Input towon: ')
 print(logpip(name,age,town))
+
+# Задание - 2
+# Создайте функцию, принимающую на вход 3 числа, и возвращающую наибольшее из них
+def mymax(a,b,c):
+    if a >=b and a >=c:
+        mmax = a
+        return mmax
+    elif b >= a and b >= c:
+        mmax = b
+        return mmax
+    elif c >= a and c >= b:
+        mmax = c
+        return mmax
+a = int(input('Input First: '))
+b = int(input('Input Second: '))
+c = int(input('Input Third: '))
+print('Max = ', mymax(a,b,c))

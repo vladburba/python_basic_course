3846c9538996590614ae7a325a7092e6beed0178
vladburba <vlad.burba@gmail.com>
2019-09-20 11:15:25 +0300

    Закончил урок 3 изи

--- hw03-easy.py
+++ hw03-easy.py
@@ -1,7 +1,8 @@
 # Задание - 1
 # Создайте функцию, принимающую на вход Имя, возраст и город проживания человека
 # Функция должна возвращать строку вида "Василий, 21 год(а), проживает в городе Москва"
-#
+
+
 def logpip(name, age, town):
      # str = name+age+town
      result = f'Name = {name} Age = {age} Town =  {town} '
@@ -15,21 +16,22 @@ print('Task_1_finished')
 # Задание - 2
 # Создайте функцию, принимающую на вход 3 числа, и возвращающую наибольшее из них
 
-# def mymax(a,b,c):
-#     if a >=b and a >=c:
-#         mmax = a
-#         return mmax
-#     elif b >= a and b >= c:
-#         mmax = b
-#         return mmax
-#     elif c >= a and c >= b:
-#         mmax = c
-#         return mmax
-# a = int(input('Input First: '))
-# b = int(input('Input Second: '))
-# c = int(input('Input Third: '))
-# print('Max = ', mymax(a,b,c))
-# print('Task_2_finished')
+
+def mymax(a,b,c):
+    if a >=b and a >=c:
+        mmax = a
+        return mmax
+    elif b >= a and b >= c:
+        mmax = b
+        return mmax
+    elif c >= a and c >= b:
+        mmax = c
+        return mmax
+a = int(input('Input First: '))
+b = int(input('Input Second: '))
+c = int(input('Input Third: '))
+print('Max = ', mymax(a,b,c))
+print('Task_2_finished')
 
 # Задание - 3
 # Создайте функцию, принимающую неограниченное количество строковых аргументов,
@@ -39,12 +41,16 @@ print('Task_1_finished')
 def func(*args):
     print(args)
     print(type(args))
-    # print(name)
     mymax = 0
+    mystr = ''
     for str in args:
         teclen = len(str)
         if teclen >= mymax:
             mymax = teclen
-    return mymax
+            mystr = str
+    print(mymax)
+    return mystr
 
-print('Максимальная длина строки = ',func('Ivan', '10fgfg', '1sdsdg', '2sdfsdf', '3', '5', '4','sdgsdgdfgdgsdgsdgsdgsd'))
+print('Строка максимальной длины = ',func('Ivan', '10fgfg', '1sdsdg', '2sdfsdf', '3', '5', '4','sdgsdgdgывпы',
+                                         'ыфвафывафывафывафываывава', 'sdgasdgasdgfasdgasdgasdgasdgasdgas'))
+print('Task_3_finished')

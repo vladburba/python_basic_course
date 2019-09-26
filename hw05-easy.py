e3076b88175e1511cdf3b13e2598959ea9bb6368
vladburba <vlad.burba@gmail.com>
2019-09-26 23:19:53 +0300

    easy 2 done

--- hw05-easy.py
+++ hw05-easy.py
@@ -4,19 +4,27 @@
 # И второй скрипт, удаляющий эти папки.
 
 import os
-print('os.name = ', os.name)
-print('os.getcwd() = ', os.getcwd())
-new_dir = input('Input name new dir: ')
-dir_path = os.path.join(os.getcwd(), new_dir)
-print('NewDir = ', dir_path)
-try:
-    os.mkdir(dir_path)
-except FileExistsError:
-    print('Такая директория уже существует')
-print('Task1_finished')
+# print('os.name = ', os.name)
+# print('os.getcwd() = ', os.getcwd())
+# new_dir = input('Input name new dir: ')
+# dir_path = os.path.join(os.getcwd(), new_dir)
+# print('NewDir = ', dir_path)
+# try:
+#     os.mkdir(dir_path)
+# except FileExistsError:
+#     print('Такая директория уже существует')
+# print('Task1_finished')
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+# print('os.getcwd() = ', os.getcwd())
+mypath = os.getcwd()
+# print(os.listdir(path="."))
+cur_dir = os.listdir(path=".")
+print(cur_dir)
+onlyfiles = [f for f in cur_dir if os.path.isfile(os.path.join(mypath, f))]
+print(onlyfiles)
+
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.

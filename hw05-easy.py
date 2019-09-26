8c1bb4f2bd9ed29101b75b77cdebf8d67cf9ae3a
vladburba <vlad.burba@gmail.com>
2019-09-26 23:53:15 +0300

    easy 3 done

--- hw05-easy.py
+++ hw05-easy.py
@@ -4,6 +4,8 @@
 # И второй скрипт, удаляющий эти папки.
 
 import os
+import sys
+import shutil
 # print('os.name = ', os.name)
 # print('os.getcwd() = ', os.getcwd())
 # new_dir = input('Input name new dir: ')
@@ -17,14 +19,18 @@ import os
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-# print('os.getcwd() = ', os.getcwd())
-mypath = os.getcwd()
-# print(os.listdir(path="."))
-cur_dir = os.listdir(path=".")
-print(cur_dir)
-onlyfiles = [f for f in cur_dir if os.path.isfile(os.path.join(mypath, f))]
-print(onlyfiles)
+# mypath = os.getcwd()
+# cur_dir = os.listdir(path=mypath)
+# print(cur_dir)
+# onlyfiles = [f for f in cur_dir if os.path.isfile(os.path.join(mypath, f))]
+# print(onlyfiles)
 
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+print('sys.argv = ', sys.argv)
+file1 = sys.argv[0]
+file2 = file1 + '.copy'
+print(file1, file2)
+shutil.copy(file1, file2)
+print(os.listdir(path=os.getcwd()))
